406d87fdb743dee341dd0410f1b0f6b9b5f8984f
Nick Valdez <nicholastvaldez@gmail.com>
2024-10-28 11:44:57 -0500

    Welcome screen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,11 +10,6 @@ export {
   ErrorBoundary,
 } from "expo-router"
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-}
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
@@ -43,5 +38,9 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  return <Stack></Stack>
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,83 @@
-import { View, Text } from "react-native"
+import Colors from "@/constants/Colors"
+import { defaultStyles } from "@/constants/Styles"
+import { useAssets } from "expo-asset"
+import { ResizeMode, Video } from "expo-av"
+import { Link } from "expo-router"
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 
 const Page = () => {
+  const [assets] = useAssets([require("@/assets/videos/intro.mp4")])
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      {assets && (
+        <Video
+          resizeMode={ResizeMode.COVER}
+          isMuted
+          isLooping
+          shouldPlay
+          source={{ uri: assets[0].uri }}
+          style={styles.video}
+        />
+      )}
+      <View style={{ marginTop: 80, padding: 20 }}>
+        <Text style={styles.header}>Ready to change the way you money?</Text>
+      </View>
+      <View style={styles.buttons}>
+        <Link
+          href={"/login"}
+          asChild
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.dark },
+          ]}
+        >
+          <TouchableOpacity>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Login
+            </Text>
+          </TouchableOpacity>
+        </Link>
+        <Link
+          href={"/signup"}
+          asChild
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: "#fff" },
+          ]}
+        >
+          <TouchableOpacity>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign Up</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginBottom: 60,
+    paddingHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: "900",
+    color: "white",
+    textTransform: "uppercase",
+  },
+})
 
 export default Page
